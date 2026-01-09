2d517924373c5b55870cfda6566489b9a7f1ad05
jun <junc76@gmail.com>
2026-01-09 22:01:40 +0800

    Add application icon support for Windows executable and update main window icon handling

--- CustomTitleBar/main.cpp
+++ CustomTitleBar/main.cpp
@@ -1,10 +1,15 @@
 #include <QApplication>
+#include <QIcon>
 #include "mainwindow.h"
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+    QIcon appIcon(":/icons/app.ico");
+    if (appIcon.isNull()) appIcon = QIcon(":/icons/app.png");
+    app.setWindowIcon(appIcon);
     MainWindow w;
+    w.setWindowIcon(appIcon);
     w.resize(800, 600);
     w.show();
     return app.exec();
